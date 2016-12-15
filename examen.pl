c1b3f4d727ff73384b5155186e619f03b7cdccdf
G1-16 <G1-16>
2016-12-14 16:48:31 -0800

    examen

---
+++ examen.pl
@@ -0,0 +1,50 @@
+es_padre(jose_arcadio_buendia, aureliano_buendia).
+es_padre(jose_arcadio_buendia, jose_arcadio).
+es_padre(jose_arcadio_buendia, amaranta).
+
+es_madre(ursula_inguaran, aureliano_buendia).
+es_madre(ursula_inguaran, jose_arcadio).
+es_madre(ursula_inguaran, amaranta).
+
+
+
+es_padre(coronel_aureliano_buendia, aureliano_jose).
+es_madre(pilar_temera, aureliano_jose).
+
+
+
+es_padre(jose_arcadio, arcadio).
+es_madre(pilar_temera, arcadio).
+
+
+
+es_padre(arcadio, aureliano_II).
+es_padre(arcadio, jose_arcadio_II).
+es_padre(arcadio, remedios).
+
+es_madre(sofia_piedad, aureliano_II).
+es_madre(sofia_piedad, jose_arcadio_II).
+es_madre(sofia_piedad, remedios).
+
+
+
+es_padre(aureliano_II, renata_remedios).
+es_padre(aureliano_II, amaranta_ursula).
+es_padre(aureliano_II, Jose_arcadio).
+
+es_madre(fernanda_carpio, renata_remedios).
+es_madre(fernanda_carpio, amaranta_ursula).
+es_madre(fernanda_carpio, Jose_arcadio).
+
+
+
+es_padre(mauricio_babilonia, aureliano_babilonia).
+es_madre(renata_remedios, aureliano babilonia).
+
+son_esposos(jose_arcadio_buendia, Ursula_inguaran).
+son_esposos(aureliano_buendia, remedios_moscote).
+son_esposos(jose_arcadio, rebeca).
+son_esposos(aureliano_II, fernanda_carpio).
+son_esposos(amaranta_ursula, gaston).
+
+
